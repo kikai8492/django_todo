eb6898876225970ce8d9a1ef0eb748cc03c1f24d
shuto kikai <yysskikai04@icloud.com>
2024-01-10 19:03:00 +0900

    method

--- todo/models.py
+++ todo/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class TodoModel(models.Model):
   title = models.CharField(max_length=100)
   memo = models.TextField()
+
+  def __str__(self):
+    return self.title
+    

dbba7068131e803ff4893e5787250180e525d27e
shuto kikai <yysskikai04@icloud.com>
2024-01-12 20:42:46 +0900

    init

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,9 @@
 
 from django.urls import path, include
 from .views import TodoList
+from .views import TodoDetail
+
 urlpatterns = [
-    path('list/', TodoList.as_view()),
+  path('list/', TodoList.as_view()),
+  path('detail/<int:pk>', TodoDetail.as_view()),
 ]

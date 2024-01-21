7c7f146f0bb911fe95f2affdd7f47ef0299a390f
shuto kikai <yysskikai04@icloud.com>
2024-01-22 08:55:41 +0900

    update

--- todo/urls.py
+++ todo/urls.py
@@ -4,10 +4,12 @@ from .views import TodoList
 from .views import TodoDetail
 from .views import TodoCreate
 from .views import TodoDelete
+from .views import TodoShow
 
 urlpatterns = [
   path('list/', TodoList.as_view(), name='list'),
   path('detail/<int:pk>', TodoDetail.as_view(), name='detail'),
   path('create/', TodoCreate.as_view(), name='create'),
   path('delete/<int:pk>',TodoDelete.as_view(), name='delete'),
+  path('show/<int:pk>',TodoShow.as_view(), name='show'),
 ]
